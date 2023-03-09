df37e198314991e0e7bddb1ce8d5233baab55111
hicarlodacuyan <hi.carlodacuyan@gmail.com>
2023-03-09 14:32:08 +0800

    Setup initial category & item controllers

---
+++ backend/controllers/categoryController.js
@@ -0,0 +1,61 @@
+const Category = require("../models/Category");
+
+const getCategories = async (_req, res) => {
+  const categories = await Category.find({}).populate("items", {
+    name: 1,
+    description: 1,
+    price: 1,
+  });
+
+  res.json(categories);
+};
+
+const getCategory = async (req, res) => {
+  const id = req.params.id;
+  const category = await Category.findById(id).populate("items", {
+    name: 1,
+    description: 1,
+    price: 1,
+  });
+
+  res.json(category);
+};
+
+const createCategory = async (req, res) => {
+  const category = new Category({
+    name: req.body.name,
+  });
+
+  const savedCategory = await category.save();
+
+  res.status(201).json(savedCategory);
+};
+
+const editCategory = async (req, res) => {
+  const id = req.params.id;
+  const category = {
+    name: req.body.name,
+  };
+
+  const updatedCategory = await Category.findByIdAndUpdate(id, category, {
+    new: true,
+  });
+
+  res.json(updatedCategory);
+};
+
+const deleteCategory = async (req, res) => {
+  const id = req.params.id;
+
+  await Category.findByIdAndRemove(id);
+
+  res.status(204).end();
+};
+
+module.exports = {
+  getCategories,
+  getCategory,
+  createCategory,
+  editCategory,
+  deleteCategory,
+};
---
+++ backend/controllers/itemController.js
@@ -0,0 +1,65 @@
+const Item = require("../models/Item");
+const Category = require("../models/Category");
+
+const getItems = async (_req, res) => {
+  const items = await Item.find({});
+
+  res.json(items);
+};
+
+const getItem = async (req, res) => {
+  const id = req.params.id;
+  const item = await Item.findById(id);
+
+  res.json(item);
+};
+
+const createItem = async (req, res) => {
+  const { name, description, price, category } = req.body;
+  const savedCategory = await Category.findById(category);
+
+  const item = new Item({
+    name,
+    description,
+    price,
+    category: savedCategory._id,
+  });
+
+  const savedItem = await item.save();
+
+  savedCategory.items = [...savedCategory.items].concat(savedItem._id);
+  await savedCategory.save();
+
+  res.status(201).json(savedItem);
+};
+
+const editItem = async (req, res) => {
+  const { name, description, price, category } = req.body;
+  const id = req.params.id;
+  const item = {
+    name,
+    description,
+    price,
+    category,
+  };
+
+  const updatedItem = await Item.findByIdAndUpdate(id, item, { new: true });
+
+  res.json(updatedItem);
+};
+
+const deleteItem = async (req, res) => {
+  const id = req.params.id;
+
+  await Item.findByIdAndRemove(id);
+
+  res.status(204).end();
+};
+
+module.exports = {
+  getItems,
+  getItem,
+  createItem,
+  editItem,
+  deleteItem,
+};
