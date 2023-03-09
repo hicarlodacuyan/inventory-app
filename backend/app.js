bfb3bce507ea935b4c8b3c20ab11744dd9ecc2c9
hicarlodacuyan <hi.carlodacuyan@gmail.com>
2023-03-09 14:29:50 +0800

    Add items & categories routes

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const config = require("./utils/config");
+const itemRouter = require("./routes/itemRoutes");
+const categoryRouter = require("./routes/categoryRoutes");
 
 const app = express();
 const url = config.MONGODB_URI;
@@ -14,5 +16,7 @@ url ? connectToDB(url) : console.log("Error connecting to DB");
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/items", itemRouter);
+app.use("/api/categories", categoryRouter);
 
 module.exports = app;
