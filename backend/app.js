48f9cd07da6d289d39b0e04d44652f9d94d0e725
hicarlodacuyan <hi.carlodacuyan@gmail.com>
2023-03-09 11:57:08 +0800

    Setup basic db connection

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
+const config = require("./utils/config");
 
 const app = express();
+const url = config.MONGODB_URI;
+const connectToDB = async (url) => {
+  await mongoose.connect(url);
+  console.log(`Connected to DB`);
+};
+
+url ? connectToDB(url) : console.log("Error connecting to DB");
 
 app.use(cors());
 
