f92d9cd5cfe75e33bd61aa061932a62591e7fe89
hicarlodacuyan <hi.carlodacuyan@gmail.com>
2023-03-10 10:47:08 +0800

    Include go back to Home action & fix styling

--- frontend/src/components/CategoriesList.jsx
+++ frontend/src/components/CategoriesList.jsx
@@ -23,10 +23,16 @@ const CategoriesList = () => {
   }
 
   return (
-    <ul className="flex gap-4">
+    <ul className="flex flex-col md:flex-row gap-4 text-white flex-wrap">
       {categories.map((category) => (
-        <li className="text-4xl p-8 bg-red-300" key={category.id}>
-          <Link to={`/categories/${category.id}`}>{category.name}</Link>
+        <li className="flex-1 bg-blue-300 md:max-w-md" key={category.id}>
+          <div className="p-4">
+            <h2 className="text-4xl font-bold">{category.name}</h2>
+            <p className="italic">{category.items.length} variety</p>
+          </div>
+          <div className="text-center bg-blue-400 py-2">
+            <Link to={`/categories/${category.id}`}>More info &rarr;</Link>
+          </div>
         </li>
       ))}
     </ul>
