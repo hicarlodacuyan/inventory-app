c0dcad270da07e7c1813b3c29ab77454c949910b
hicarlodacuyan <hi.carlodacuyan@gmail.com>
2023-03-10 10:47:52 +0800

    Setup categories page & include back action to home

--- frontend/src/pages/Categories.jsx
+++ frontend/src/pages/Categories.jsx
@@ -1,8 +1,15 @@
+import { Link } from "react-router-dom";
 import CategoriesList from "../components/CategoriesList";
 
 const Categories = () => {
   return (
-    <div className="flex justify-center items-center gap-4 h-screen">
+    <div className="h-screen bg-slate-100 p-4">
+      <h1 className="text-2xl font-bold mb-4">
+        <Link to="/" className="font-black mr-4">
+          &larr;
+        </Link>
+        Categories
+      </h1>
       <CategoriesList />
     </div>
   );
