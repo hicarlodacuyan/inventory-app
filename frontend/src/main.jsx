25f2eccefe8386f22e52a608277ba9347bb07e04
hicarlodacuyan <hi.carlodacuyan@gmail.com>
2023-03-10 09:07:13 +0800

    Wrap App component in react router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,12 @@ import App from "./App";
 import "./index.css";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>
 );
