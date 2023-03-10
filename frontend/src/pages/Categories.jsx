a3c3fdb4890ad40d1b62678775e13d05eb735695
hicarlodacuyan <hi.carlodacuyan@gmail.com>
2023-03-10 16:24:02 +0800

    Fix loading spinner position not to cover entire page

--- frontend/src/pages/Categories.jsx
+++ frontend/src/pages/Categories.jsx
@@ -25,20 +25,18 @@ const Categories = () => {
 
   return (
     <div className="h-screen bg-slate-100 p-4">
+      <h1 className="text-2xl font-bold mb-4">
+        <Link to="/" className="font-black mr-4">
+          &larr;
+        </Link>
+        Categories
+      </h1>
+      <AddCategoryForm />
+      <hr className="my-4" />
       {isLoading ? (
         <LoadingSpinner />
       ) : (
-        <>
-          <h1 className="text-2xl font-bold mb-4">
-            <Link to="/" className="font-black mr-4">
-              &larr;
-            </Link>
-            Categories
-          </h1>
-          <AddCategoryForm />
-          <hr className="my-4" />
-          <CategoriesList categories={categories} />
-        </>
+        <CategoriesList categories={categories} />
       )}
     </div>
   );
