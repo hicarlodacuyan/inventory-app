e84ac7e476fcb61adc9739584b1c119380159570
hicarlodacuyan <hi.carlodacuyan@gmail.com>
2023-03-10 10:48:27 +0800

    Setup specific category page

--- frontend/src/pages/Category.jsx
+++ frontend/src/pages/Category.jsx
@@ -3,12 +3,13 @@ import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getCategory } from "../features/categorySlice";
 import LoadingSpinner from "../components/LoadingSpinner";
+import { Link } from "react-router-dom";
+import Items from "../components/Items";
 
 const Category = () => {
   const { id } = useParams();
-  const { currentCategory, isLoading, isError, message } = useSelector(
-    (state) => state.category
-  );
+  const { currentCategory, isLoading, isError, isSuccess, message } =
+    useSelector((state) => state.category);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -20,17 +21,18 @@ const Category = () => {
   }, []);
 
   return (
-    <div className="flex flex-col h-screen justify-center items-center">
+    <div className="h-screen bg-slate-100 p-4">
       {isLoading ? (
         <LoadingSpinner />
       ) : (
         <>
-          <h1 className="text-4xl underline">{currentCategory?.name}</h1>
-          <ul>
-            {currentCategory?.items.map((item) => (
-              <li key={item.id}>{item?.name}</li>
-            ))}
-          </ul>
+          <h1 className="text-2xl font-bold mb-4">
+            <Link to="/categories" className="font-black mr-4">
+              &larr;
+            </Link>
+            {currentCategory?.name}
+          </h1>
+          <Items items={currentCategory?.items} />
         </>
       )}
     </div>
