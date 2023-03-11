194c3d3f51e635d3fc8228f3975d164f1f22cc36
hicarlodacuyan <hi.carlodacuyan@gmail.com>
2023-03-11 17:38:27 +0800

    Make items cards rounded

--- frontend/src/components/ItemsList.jsx
+++ frontend/src/components/ItemsList.jsx
@@ -2,7 +2,10 @@ const ItemsList = ({ items }) => {
   return (
     <ul className="flex flex-col md:flex-row gap-4 text-white flex-wrap">
       {items?.map((item) => (
-        <li className="flex-1 bg-rose-300 md:max-w-md" key={item.id}>
+        <li
+          className="flex-1 bg-rose-300 md:max-w-md rounded-lg drop-shadow-lg"
+          key={item.id}
+        >
           <div className="p-4">
             <h2 className="text-4xl font-bold">
               {item.name} (${item.price})
