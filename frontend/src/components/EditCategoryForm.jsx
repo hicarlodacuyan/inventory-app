f5e3f4684bee09ace40bd7a48d72aa2ca8b43a1f
hicarlodacuyan <hi.carlodacuyan@gmail.com>
2023-03-11 17:35:52 +0800

    Create edit category form component

---
+++ frontend/src/components/EditCategoryForm.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { updateCategory } from "../features/categorySlice";
+
+const EditCategoryForm = ({ category, onCancel }) => {
+  const [newCategory, setNewCategory] = useState(category.name);
+  const dispatch = useDispatch();
+
+  const handleUpdate = async (e) => {
+    e.preventDefault();
+
+    const updatedCategory = {
+      id: category.id,
+      name: newCategory,
+    };
+
+    dispatch(updateCategory(updatedCategory));
+    onCancel();
+  };
+
+  return (
+    <form
+      onSubmit={handleUpdate}
+      className="flex flex-col gap-4 p-4 bg-white md:max-w-md rounded-lg drop-shadow-lg"
+    >
+      <h1 className="text-xl">Edit category</h1>
+      <input
+        className="bg-transparent border-2 outline-none rounded-md p-2 focus:border-blue-500"
+        type="text"
+        value={newCategory}
+        name="newCategory"
+        onChange={(e) => setNewCategory(e.target.value)}
+        required
+      />
+      <button
+        className="bg-gray-200 text-black font-bold rounded-md p-2 hover:bg-gray-300"
+        type="submit"
+      >
+        Save
+      </button>
+    </form>
+  );
+};
+
+export default EditCategoryForm;
