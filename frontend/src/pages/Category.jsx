b067239bb8fd162b515348782062182e2567e301
hicarlodacuyan <hi.carlodacuyan@gmail.com>
2023-03-12 09:09:22 +0800

    Fix bug that when the category page is refresh, the getCategories action is not being dispatch & the currentCategory can't be find as a result resulting in a reference error

--- frontend/src/pages/Category.jsx
+++ frontend/src/pages/Category.jsx
@@ -1,24 +1,37 @@
-import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { Link, useParams } from "react-router-dom";
+import { getCategories } from "../features/categorySlice";
 import ItemsList from "../components/ItemsList";
 import AddItemForm from "../components/AddItemForm";
 
 const Category = () => {
   const { id } = useParams();
   const { categories } = useSelector((state) => state.category);
+  const dispatch = useDispatch();
   const [currentCategory] = categories.filter((category) => category.id === id);
+
+  useEffect(() => {
+    if (categories.length === 0) {
+      dispatch(getCategories());
+    }
+  }, [categories, dispatch]);
 
   return (
     <div className="h-screen bg-slate-100 p-4">
-      <h1 className="text-2xl font-bold mb-4">
-        <Link to="/categories" className="font-black mr-4">
-          &larr;
-        </Link>
-        {currentCategory.name}
-      </h1>
-      <AddItemForm categories={[currentCategory]} />
-      <hr className="my-4" />
-      <ItemsList items={currentCategory.items} />
+      {currentCategory && (
+        <>
+          <h1 className="text-2xl font-bold mb-4">
+            <Link to="/categories" className="font-black mr-4">
+              &larr;
+            </Link>
+            {currentCategory.name}
+          </h1>
+          <AddItemForm categories={[currentCategory]} />
+          <hr className="my-4" />
+          <ItemsList items={currentCategory.items} />
+        </>
+      )}
     </div>
   );
 };
